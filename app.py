d3b85f4f83348516a86a639b8f6f07b85d57d562
ManuelG <manuel5.guzman1@gmail.com>
2022-04-10 12:45:00 -0400

    Final submission

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+import os
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['POSTGRES_USER']}:" \
+                                            f"{os.environ['POSTGRES_PASSWORD']}" \
+                                            f"@payment-db/{os.environ['POSTGRES_DB']}"
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    return app
+
+
+app = create_app()
 
 
 @app.route('/api/v1/home')
