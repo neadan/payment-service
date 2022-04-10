2f3cc7ac38fd9384d25e065d55a7afd4ed31b1e9
ariel201909118 <79768095+ariel201909118@users.noreply.github.com>
2022-04-10 17:23:21 -0400

    Ariel

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import os
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:" \
+                                        f"{os.environ['POSTGRES_PASSWORD']}" \
+                                        f"@purchase-db/{os.environ['POSTGRES_DB']}"
 
 
 @app.route('/api/v1/home')
